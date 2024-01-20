af640c357063031447eb57c0acea7e0b0528d2c9
SerhiiBudko <140949363+SerhiiBudko@users.noreply.github.com>
2024-01-20 21:39:35 +0100

    chaged file setting.py

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -14,23 +14,20 @@ import os
 from pathlib import Path
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = config("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = config("DEBUG", cast=bool, default=False)
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
